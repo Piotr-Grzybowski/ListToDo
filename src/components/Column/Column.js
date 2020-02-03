16e1d80af4c43a8b972570cfc8e9959f52486efc
Piotr Grzybowski <grzybek6@gmail.com>
2020-02-03 18:42:04 +0000

    change components on functional

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -6,38 +6,34 @@ import Creator from '../Creator/Creator';
 import Card from '../Card/Card';
 import { settings } from '../../data/dataStore';
 
-class Column extends React.Component {
 
-  static propTypes = {
-    title: PropTypes.string,
-    cards: PropTypes.array,
-    icon: PropTypes.string,
-    addCard: PropTypes.func,
-  }
-  
-  static defaultProps = {
-    icon: settings.defaultColumnIcon,
-  }
 
-  render() {
-    const {title, icon, cards, addCard} = this.props;
-    return (
-      <section className={styles.component}>
-        <h3 className={styles.title}>
-          {title}
-          <span className={styles.icon}>
-            <Icon name={icon}/>
-          </span>
-        </h3>
-        {cards.map(cardData => (
-          <Card key={cardData.id} {...cardData} />
-        ))}
-        <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={addCard} />
-        </div>
-      </section>
-    );
-  }
-}
+const Column = ({title, icon, cards, addCard}) => (
+  <section className={styles.component}>
+    <h3 className={styles.title}>
+      {title}
+      <span className={styles.icon}>
+        <Icon name={icon}/>
+      </span>
+    </h3>
+    {cards.map(cardData => (
+      <Card key={cardData.id} {...cardData} />
+    ))}
+    <div className={styles.creator}>
+      <Creator text={settings.cardCreatorText} action={addCard} />
+    </div>
+  </section>
+);
+
+Column.propTypes = {
+  title: PropTypes.string,
+  cards: PropTypes.array,
+  icon: PropTypes.string,
+  addCard: PropTypes.func,
+};
+
+Column.defaultProps = {
+  icon: settings.defaultColumnIcon,
+};
 
 export default Column;
